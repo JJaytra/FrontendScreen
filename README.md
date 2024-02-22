cdf6a61a37605212868c054a8334db4d888721c4
Jamie <jaytra23@gmail.com>
2024-02-22 19:48:10 +1100

    fixed navbar scaling on smaller screen

--- README.md
+++ README.md
@@ -1,18 +1,29 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+Hello~
 
-## Getting Started
+Please run
 
-First, run the development server:
+1. npm install
 
-```bash
+2. Then
+
 npm run dev
+
 # or
+
 yarn dev
+
 # or
+
 pnpm dev
+
 # or
+
 bun dev
-```
+
+3.
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
+Yay
+
+This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
--- app/(components)/Nav.tsx
+++ app/(components)/Nav.tsx
@@ -14,28 +14,28 @@ const Nav = () => {
     <div className="w-20 bg-nav p-4 h-full flex flex-col justify-between">
       {/* container 1 */}
       <div className="flex flex-col items-center">
-        <div className="mb-4 ">
+        <div className="mb-4 border-b-2 pb-2">
           <Image src="../logo.svg" alt="logo" width={40} height={40} />
         </div>
-        <div className="mt-8 p-3 rounded-lg hover:bg-slate-200">
+        <div className="mt-1/8 p-3 rounded-lg hover:bg-slate-200">
           <IconBook />
         </div>
-        <div className="mt-4 p-3 rounded-lg hover:bg-slate-200">
+        <div className="mt-1/8 p-3 rounded-lg hover:bg-slate-200">
           <IconShieldCheck />
         </div>
       </div>
       {/* container 2 */}
       <div className="flex flex-col items-center">
-        <div className="mt-auto mb-10 p-3 rounded-lg hover:bg-slate-200">
+        <div className="mt-auto mb-1/8 p-3 rounded-lg hover:bg-slate-200">
           <IconLayoutSidebarLeftExpandFilled />
         </div>
-        <div className="mb-10 p-3 rounded-lg hover:bg-slate-200">
+        <div className="mb-1/8 p-3 rounded-lg hover:bg-slate-200">
           <IconUserCircle />
         </div>
-        <div className="mb-10 p-3 rounded-lg hover:bg-slate-200">
+        <div className="mb-1/8 p-3 rounded-lg hover:bg-slate-200">
           <IconMoon />
         </div>
-        <div className="mb-10 p-3 rounded-lg hover:bg-slate-200">
+        <div className="mb-1/8 p-3 rounded-lg hover:bg-slate-200">
           <IconLogout />
         </div>
       </div>
